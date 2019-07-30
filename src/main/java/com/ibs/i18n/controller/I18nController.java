a31703016eecb8332788ec8c1145d8543b18fecc
DougLei <953034206@qq.com>
2019-07-30 10:09:33 +0800

    t

--- src/main/java/com/ibs/i18n/controller/I18nController.java
+++ src/main/java/com/ibs/i18n/controller/I18nController.java
@@ -1,9 +1,28 @@
 package com.ibs.i18n.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ibs.i18n.service.I18nService;
+import com.ibs.response.IResponse;
+import com.ibs.response.impl.dve.ResponseContext;
+
 /**
  * 
  * @author DougLei
  */
+@RestController
+@RequestMapping("/i18n")
 public class I18nController {
-
+	
+	@Autowired
+	private I18nService service;
+	
+	@RequestMapping("/query/{code}")
+	public IResponse query(@PathVariable String code) {
+		
+		return ResponseContext.getResponseInstance();
+	}
 }
--- src/main/java/com/ibs/i18n/entity/Entity.java
+++
@@ -1,7 +0,0 @@
-package com.ibs.i18n.entity;
-
-import com.ibs.parent.code.entity.BasicEntity;
-
-public class Entity extends BasicEntity{
-
-}
