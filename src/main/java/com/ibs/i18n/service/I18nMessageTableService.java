baeb05d3ccc863788c0ce541e90f4f8e3f5d978c
DougLei <953034206@qq.com>
2019-08-07 18:01:54 +0800

    t

--- src/main/java/com/ibs/i18n/service/I18nMessageTableService.java
+++ src/main/java/com/ibs/i18n/service/I18nMessageTableService.java
@@ -1,14 +1,7 @@
 package com.ibs.i18n.service;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.douglei.orm.context.transaction.component.TransactionComponent;
-import com.douglei.tools.utils.ExceptionUtil;
+import com.douglei.tools.instances.reader.ProjectConfigurationResourceReader;
 
 /**
  * 
@@ -16,22 +9,10 @@ import com.douglei.tools.utils.ExceptionUtil;
  */
 @TransactionComponent
 public class I18nMessageTableService {
-	private static final Logger logger = LoggerFactory.getLogger(I18nMessageTableService.class);
 	
 	private static final String tableNamePrefix = "I18N_MESSAGE_";// 表名前缀
 	private static final String templatePath = "mappings/I18nMessage.tmp.xml.template";// 模板路径
-	private static final String tableMappingTemplate;// 表映射模板
-	static {
-		StringBuilder tmt = new StringBuilder(1305);
-		try(BufferedReader br = new BufferedReader(new InputStreamReader(I18nMessageTableService.class.getClassLoader().getResourceAsStream(templatePath)))){
-			while(br.ready()) {
-				tmt.append(br.readLine().trim());
-			}
-		} catch (IOException e) {
-			logger.error("读取国际化消息表模板文件["+templatePath+"]时出现异常: {}", ExceptionUtil.getExceptionDetailMessage(e));
-		}
-		tableMappingTemplate = tmt.toString();
-	}
+	private static final String tableMappingTemplate = new ProjectConfigurationResourceReader(templatePath).readAll(1360).toString();// 表映射模板
 	
 	/**
 	 * 获得国际化消息表的表名
