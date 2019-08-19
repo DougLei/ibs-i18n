ef566676f265684dc9bd6e362e09bc566505c771
DougLei <953034206@qq.com>
2019-08-19 15:29:31 +0800

    t

---
+++ src/main/java/com/ibs/i18n/entity/I18nDynamicTable.java
@@ -0,0 +1,11 @@
+package com.ibs.i18n.entity;
+
+import com.ibs.dynamic.table.DynamicTableEntity;
+
+/**
+ * 国际化动态表服务
+ * @author DougLei
+ */
+public class I18nDynamicTable extends DynamicTableEntity{
+	private static final long serialVersionUID = -8955875155599659302L;
+}
