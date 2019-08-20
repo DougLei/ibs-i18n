f3df3eb2fc790d1808c140c4389fed35a1378a19
DougLei <953034206@qq.com>
2019-08-20 18:33:06 +0800

    t

--- src/main/java/com/ibs/i18n/controller/I18nController.java
+++ src/main/java/com/ibs/i18n/controller/I18nController.java
@@ -1,11 +1,18 @@
 package com.ibs.i18n.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.douglei.tools.utils.Collections;
+import com.ibs.i18n.entity.I18nMessage;
 import com.ibs.i18n.service.I18nQueryService;
 import com.ibs.i18n.service.I18nUpdateService;
+import com.ibs.response.Response;
+import com.ibs.response.ResponseContext;
 
 /**
  * 
@@ -21,12 +28,25 @@ public class I18nController {
 	@Autowired
 	private I18nUpdateService updateService;
 	
+	/**
+	 * 添加国际化消息
+	 * @param message
+	 * @return
+	 */
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public Response add(I18nMessage message) {
+		adds(Collections.toList(I18nMessage.class, message));
+		return ResponseContext.getFinalResponse();
+	}
 	
-	
-	
-	
-	
-	
-	
-	
+	/**
+	 * 批量添加国际化消息
+	 * @param messages
+	 * @return
+	 */
+	@RequestMapping(value="/adds", method=RequestMethod.POST)
+	public Response adds(List<I18nMessage> messages) {
+		updateService.adds(messages);
+		return ResponseContext.getFinalResponse(true);
+	}
 }
--- src/main/java/com/ibs/i18n/service/I18nUpdateService.java
+++ src/main/java/com/ibs/i18n/service/I18nUpdateService.java
@@ -1,8 +1,13 @@
 package com.ibs.i18n.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.douglei.orm.context.SessionContext;
+import com.douglei.orm.context.transaction.component.Transaction;
 import com.douglei.orm.context.transaction.component.TransactionComponent;
+import com.ibs.i18n.entity.I18nMessage;
 
 /**
  * 
@@ -13,6 +18,15 @@ public class I18nUpdateService {
 
 	@Autowired
 	private I18nMessageTableService tableService;
-	
-	
+
+	/**
+	 * 添加国际化消息
+	 * @param messages
+	 */
+	@Transaction
+	public void adds(List<I18nMessage> messages) {
+		messages.forEach(message -> {
+			SessionContext.getTableSession().save(message);
+		});
+	}
 }
