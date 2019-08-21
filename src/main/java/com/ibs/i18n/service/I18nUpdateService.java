9a29c9e110788d3d54fd24084a99744d7bfea7e3
DougLei <953034206@qq.com>
2019-08-21 09:00:50 +0800

    t

--- src/main/java/com/ibs/i18n/service/I18nUpdateService.java
+++ src/main/java/com/ibs/i18n/service/I18nUpdateService.java
@@ -1,15 +1,12 @@
 package com.ibs.i18n.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.douglei.orm.context.SessionContext;
 import com.douglei.orm.context.transaction.component.Transaction;
 import com.douglei.orm.context.transaction.component.TransactionComponent;
 import com.ibs.i18n.entity.I18nMessage;
-import com.ibs.response.ResponseContext;
 
 /**
  * 
@@ -27,21 +24,21 @@ public class I18nUpdateService {
 	 */
 	@Transaction
 	public void adds(List<I18nMessage> messages) {
-		ValidatorHandler handler = SessionContext.getTableSession().getValidatorHandler(I18nMessage.class);
+//		ValidatorHandler handler = SessionContext.getTableSession().getValidatorHandler(I18nMessage.class);
+//		
+//		List<I18nMessage> ok_messages = new ArrayList<I18nMessage>(messages.size());
+//		messages.forEach(message -> {
+//			VaidationResult result = handler.doValidate(message);
+//			if(result != null) {
+//				ResponseContext.addValidation(data, code, params);
+//				return;
+//			}
+//			ok_messages.add(message);
+//		});
 		
-		List<I18nMessage> ok_messages = new ArrayList<I18nMessage>(messages.size());
-		messages.forEach(message -> {
-			VaidationResult result = handler.doValidate(message);
-			if(result != null) {
-				ResponseContext.addValidation(data, code, params);
-				return;
-			}
-			ok_messages.add(message);
-		});
 		
-		
-		ok_messages.forEach(message -> {
-			SessionContext.getTableSession().save(message);
-		});
+//		ok_messages.forEach(message -> {
+//			SessionContext.getTableSession().save(message);
+//		});
 	}
 }
