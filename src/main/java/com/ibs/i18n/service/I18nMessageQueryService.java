79e12c0ac42ca5dad6f3eff8f3652d8039d286b6
DougLei <953034206@qq.com>
2019-09-05 11:54:20 +0800

    t

--- src/main/java/com/ibs/i18n/service/I18nMessageQueryService.java
+++ src/main/java/com/ibs/i18n/service/I18nMessageQueryService.java
@@ -1,7 +1,5 @@
 package com.ibs.i18n.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.douglei.orm.context.transaction.component.TransactionComponent;
 
 /**
@@ -10,9 +8,5 @@ import com.douglei.orm.context.transaction.component.TransactionComponent;
  */
 @TransactionComponent
 public class I18nMessageQueryService {
-	
-	@Autowired
-	public I18nDynamicTableService tableService;
-	
 	
 }
