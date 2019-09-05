874b056db8422e705ee22848b7710e2c019fed47
DougLei <953034206@qq.com>
2019-09-05 23:54:58 +0800

    t

--- src/main/java/com/ibs/i18n/controller/I18nDynamicTableController.java
+++ src/main/java/com/ibs/i18n/controller/I18nDynamicTableController.java
@@ -5,10 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ibs.dynamic.table.ValidateProjectIdNotNull;
 import com.ibs.i18n.service.I18nDynamicTableService;
 import com.ibs.parent.code.controller.BasicController;
-import com.ibs.parent.code.validator.DataValidationResult;
 import com.ibs.response.Response;
 import com.ibs.response.ResponseContext;
 
@@ -19,35 +17,27 @@ import com.ibs.response.ResponseContext;
 @RestController
 @RequestMapping("/i18n/dynamic/table")
 public class I18nDynamicTableController extends BasicController{
-	private static final ValidateProjectIdNotNull validateProjectIdNotNullWhenInitial = new ValidateProjectIdNotNull("i18n", (byte)1);
-	private static final ValidateProjectIdNotNull validateProjectIdNotNullWhenDestroy = new ValidateProjectIdNotNull("i18n", (byte)0);
 	
 	@Autowired
 	private I18nDynamicTableService dynamicTableService;
 	
 	/**
 	 * 初始化指定项目的I18nMessage表
-	 * @param projectId
 	 * @return
 	 */
-	@RequestMapping(value="/initial/{projectId}", method=RequestMethod.GET)
-	public Response initial(String projectId) {
-		if(validateByValidator(projectId, validateProjectIdNotNullWhenInitial) == DataValidationResult.SUCCESS) {
-			dynamicTableService.initial(projectId);
-		}
+	@RequestMapping(value="/initial", method=RequestMethod.GET)
+	public Response initial() {
+		dynamicTableService.initial();
 		return ResponseContext.getFinalResponse();
 	}
 	
 	/**
 	 * 销毁指定项目的I18nMessage表
-	 * @param projectId
 	 * @return
 	 */
-	@RequestMapping(value="/destroy/{projectId}", method=RequestMethod.GET)
-	public Response destroy(String projectId) {
-		if(validateByValidator(projectId, validateProjectIdNotNullWhenDestroy) == DataValidationResult.SUCCESS) {
-			dynamicTableService.destroy(projectId);
-		}
+	@RequestMapping(value="/destroy", method=RequestMethod.GET)
+	public Response destroy() {
+		dynamicTableService.destroy();
 		return ResponseContext.getFinalResponse();
 	}
 }
--- src/main/java/com/ibs/i18n/service/I18nDynamicTableService.java
+++ src/main/java/com/ibs/i18n/service/I18nDynamicTableService.java
@@ -1,5 +1,7 @@
 package com.ibs.i18n.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.douglei.orm.context.transaction.component.Transaction;
 import com.douglei.orm.context.transaction.component.TransactionComponent;
 import com.douglei.tools.instances.reader.ResourcesReader;
@@ -11,26 +13,25 @@ import com.ibs.dynamic.table.DynamicTableService;
  */
 @TransactionComponent
 public class I18nDynamicTableService {
-	private static final String i18nMessageTableTemplate = new ResourcesReader("mappings/I18nMessage.tmp.xml.template").readAll(1500).toString();// 表映射模板
+	private static final String i18nMessageTableTemplate = new ResourcesReader("mappings/I18nMessage.tmp.xml.template").readAll(1500).toString();
+	private static final String i18nMessageTableCode = "I18N_MESSAGE_";
 	
+	@Autowired
 	private DynamicTableService service;
 	
 	/**
 	 * 初始化指定项目的动态表
-	 * @param projectId
 	 */
 	@Transaction
-	public synchronized void initial(String projectId) {
-		service.initial("i18n", projectId, i18nMessageTableTemplate);
+	public synchronized void initial() {
+		service.initial("i18n", i18nMessageTableTemplate);
 	}
 	
 	/**
 	 * 销毁指定项目的动态表
-	 * @param projectId
 	 */
 	@Transaction
-	public synchronized void destroy(String projectId) {
-		// TODO Auto-generated method stub
-		
+	public synchronized void destroy() {
+		service.destroy("i18n", i18nMessageTableCode);
 	}
 }
