aded6a21beb5508e95cacd0e9e92976ab7fb3dc8
DougLei <953034206@qq.com>
2020-05-08 00:53:00 +0800

    t

--- src/main/java/com/ibs/IbsI18nApplication.java
+++ src/main/java/com/ibs/IbsI18nApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
+import com.ibs.components.filters.dynamic.table.DynamicTableConfigurationProperties;
 import com.ibs.i18n.I18nUtil;
 import com.ibs.parent.code.service.dynamic.table.DynamicTableService;
 
@@ -23,8 +24,12 @@ public class IbsI18nApplication {
 	@Autowired
 	private I18nUtil util;
 	
+	@Autowired
+	private DynamicTableConfigurationProperties dynamicTableConfig;
+	
 	@Bean
 	public void start() {
+		dynamicTableConfig.processProjectInfo("i18n");
 		service.start(util.getMappingTemplates());
 	}
 	
